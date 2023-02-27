bb013b62ae2ca0fc0d73b76ec2b33ea53424286e
nadir <nadir@MACHINE>
2023-02-27 12:24:00 +0400

    Add project files.

---
+++ JobApplication.UnitTest/JobApplicationTest.cs
@@ -0,0 +1,30 @@
+﻿
+
+using JobApplicationLibrary;
+
+namespace JobApplication.UnitTest
+{
+    public class JobApplicationTest
+    {
+        [Test]
+        public void ApplicationEvalutor_WithUnderAge_UnderCondition()
+        {
+            // Arrange
+            ApplicationEvaluator evaluator = new ApplicationEvaluator();
+            JobApplicationLibrary.Models.JobApplication form = new()
+            {
+                Applicant = new()
+                {
+                    Age = 18
+                }
+            };
+
+
+            // Act
+            var result = evaluator.Evalute(form);
+
+            // Assert
+            Assert.AreEqual(ApplicationResult.AutoRejected, result);
+        }
+    }
+}
